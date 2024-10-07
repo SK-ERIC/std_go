c69309efc1a32b357dfee84d34a3b85137e6f832
SK-ERIC <974264052@qq.com>
2024-10-07 20:27:38 +0800

    func

--- learngo/func.go
+++ learngo/func.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"runtime"
+)
 
 func eval(a, b int, op string) (int, error) {
 	switch op {
@@ -20,8 +25,29 @@ func eval(a, b int, op string) (int, error) {
 }
 
 // 13 / 3 = 4...1
+// 函数返回多个值时可以起名字；仅用于非常简单的函数；对于调用者而言没有区别
 func div(a, b int) (q, r int) {
 	return a / b, a % b
+}
+
+func apply(op func(int, int) int, a, b int) int {
+	p := reflect.ValueOf(op).Pointer()
+	opName := runtime.FuncForPC(p).Name()
+	fmt.Printf("Calling function %c with args (%d, %d) \n", opName, a, b)
+	return op(a, b)
+}
+
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+// 可变参数列表
+func sum(numbers ...int) int {
+	s := 0
+	for i := range numbers {
+		s += numbers[i]
+	}
+	return s
 }
 
 func main() {
@@ -32,4 +58,8 @@ func main() {
 	}
 	q, r := div(13, 3)
 	fmt.Println(q, r)
+
+	fmt.Println(apply(pow, 3, 4))
+
+	fmt.Println(sum(1, 2, 3, 4, 5))
 }
